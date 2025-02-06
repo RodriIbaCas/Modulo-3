8d1238ced2a98c3cf433f40de7249fbe84bd19c5
Rodrigo <a01625569@tec.mx>
2025-02-06 15:28:08 -0600

    Agrega ejemplos de tipos de datos en JS

---
+++ Clase 01/tipos-de-dato.js
@@ -0,0 +1,16 @@
+
+console.log(typeof 42); 
+console.log(typeof 'Veinticinco'); 
+console.log(typeof -666); 
+console.log(typeof true); 
+console.log(typeof 0); 
+console.log(typeof ''); 
+console.log(typeof null); 
+console.log(typeof undefined); 
+console.log(typeof FALSE); 
+
+// Otros ejemplos adicionales
+console.log(typeof function(){});
+console.log(typeof NaN); 
+console.log(typeof Symbol('id'));
+console.log(typeof BigInt(9007199254740991)); 
