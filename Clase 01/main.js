632ae46385ffccbb1646f4d6f357fe3aec24e1eb
Rodrigo <a01625569@tec.mx>
2025-02-06 15:24:01 -0600

    subiendo primeros archivos

---
+++ Clase 01/main.js
@@ -0,0 +1,36 @@
+console.log('Hola Mundo');
+
+//Tipos de datos permitidos
+let nombre = "Rodri";
+let edad = 21;
+let esMayorDeEdad = true;
+let saldo;
+let cuenta = null;
+let = Symbol('id');
+let numeroGrande = 234234234234234324324n;
+
+console.log(typeof cuenta);
+/**
+ * Variable que funciona como objeto
+ * 
+ * Guarda info de una persona
+ */
+
+let persona = {
+    nombre: "Ivan",
+    edad: 23,
+    nacionalidad: "Mexicano"
+}
+
+console.log(persona.edad);
+
+let numeros = [1,2,3,4,5]
+console.log(numeros[0]);
+
+let suma = function suma(a,b) {return a = b}
+
+console.log(suma(2,5))
+
+let fecha = new Date();
+console.log(fecha);
+console.log(typeof Date());
